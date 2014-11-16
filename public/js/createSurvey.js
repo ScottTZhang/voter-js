2f5c91ff4b73d9dafb089265ee0bdf7196ef4810
Ting Zhang <tingzhang2013@gmail.com>
2014-11-16 02:31:26 -0600

    dynamically adding questions and items

--- public/js/createSurvey.js
+++ public/js/createSurvey.js
@@ -1,4 +1,39 @@
 $(function() {
+  $(document).on("click", ".add_item_class", function(e) {
+    console.log(e.target);
+    $(e.target).parent().parent().before(
+        '<div class="form-group item">\
+          <label for="forItem1" class = "col-sm-2 control-label">Item</label>\
+          <div class="col-sm-10">\
+            <input name="item" class="form-control" type="text" placeholder="item"/>\
+          </div>\
+        </div>');
+  });
+
+  $("#add_question_btn").click(function() {
+    $(".add_question").before(
+      '<div class="question">\
+        <div class="form-group">\
+          </br>\
+          <label for="forQuestion1" class = "col-sm-2 control-label">Question</label>\
+          <div class="col-sm-10">\
+            <input name="question" class="form-control" type="text" placeholder="Describe your question"/>\
+          </div>\
+        </div>\
+        <div class="form-group item">\
+          <label for="forItem1" class = "col-sm-2 control-label">ItemA</label>\
+          <div class="col-sm-10">\
+            <input name="item" class="form-control" type="text" placeholder="item"/>\
+          </div>\
+        </div>\
+        <div class="form-group add_item">\
+          <div class="col-sm-offset-2 col-sm-10">\
+            <button type="button" class="btn btn-default add_item_class">Add Item</button>\
+          </div>\
+        </div>\
+      </div>');
+
+  });
 
   $("#create_survey_btn").click(function() {
     var survey = {};
